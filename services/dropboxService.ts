c970e2c6996a4fae31aa09f912f6605de43ac8fa
Lou-Janssen <47315337+Lou-Janssen@users.noreply.github.com>
2022-12-10 13:51:45 +0100

    fix met dropbox library v10

--- services/dropboxService.ts
+++ services/dropboxService.ts
@@ -1,34 +1,30 @@
 import fetch from 'node-fetch';
-import { Dropbox } from 'dropbox';
-import * as https from "https";
+import {Dropbox} from 'dropbox';
 
 const accessToken = process.env.DROPBOX;
 const clientId = process.env.DROPBOXCLIENT;
 const clientSecret = process.env.DROPBOXSECRET;
-const refreshToken = process.env.REFRESHTOKEN
+const refreshToken = process.env.REFRESHTOKEN;
+const pathRoot = 'Snipe-bot/';
 
 if (!accessToken || !clientId || !clientSecret || !refreshToken) throw Error('DROPBOX environment variable not defined!');
 
-type RefreshToken = {
-  access_token: string;
-  token_type: string;
-  expires_in: string;
-};
-
-const dbx = new Dropbox({ accessToken, clientId, clientSecret,refreshToken,fetch });
+const dbx = new Dropbox({ accessToken, clientId, clientSecret,refreshToken,fetch});
 const UPLOAD_FILE_SIZE_LIMIT = 150 * 1024 * 1024;
 
 export async function uploadFile(file: string, content: string): Promise<void> {
   if (content.length >= UPLOAD_FILE_SIZE_LIMIT) return;
-  const response = await dbx.filesUpload({ path: `/${file}`, contents: content, mode: { '.tag': 'overwrite' } });
+  await dbx.filesUpload({ path: `/${file}`, contents: content, mode: { '.tag': 'overwrite' } });
 }
 
 export async function downloadFile<T>(file: string): Promise<T> {
 
-  var response = await dbx.filesDownload({ path: `/${file}` }) as { fileBinary?: Buffer };
-  if (!response.fileBinary) {
+  var response = await dbx.filesDownload({ path: `/${file}` });
+  var binary = response.result as { fileBinary?: Buffer }
+  console.log(binary.fileBinary);
+  if (!binary.fileBinary) {
     throw new Error(`File not found: ${file}`);
   }
 
-  return JSON.parse(response.fileBinary.toString('utf8')) as T;
+  return JSON.parse(binary.fileBinary.toString('utf8')) as T;
 }
